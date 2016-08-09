f0bb4c3d6a01be3940cd95c4a8c4dc73f33d1167
ToDou <yytodou123@gmail.com>
2016-08-09 16:05:59 +0800

    fix divider item last gone

--- app/src/main/java/com/loopeer/codereader/ui/decoration/DividerItemDecoration.java
+++ app/src/main/java/com/loopeer/codereader/ui/decoration/DividerItemDecoration.java
@@ -113,7 +113,7 @@ public class DividerItemDecoration extends RecyclerView.ItemDecoration {
         final int right = parent.getWidth() - parent.getPaddingRight();
 
         final int childCount = parent.getChildCount();
-        for (int i = 0; i < childCount; i++) {
+        for (int i = 0; i < childCount - 1; i++) {
             final View child = parent.getChildAt(i);
             final RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
             final int top = child.getBottom() + params.bottomMargin +
@@ -131,7 +131,7 @@ public class DividerItemDecoration extends RecyclerView.ItemDecoration {
         final int bottom = parent.getHeight() - parent.getPaddingBottom();
 
         final int childCount = parent.getChildCount();
-        for (int i = 0; i < childCount; i++) {
+        for (int i = 0; i < childCount - 1; i++) {
             final View child = parent.getChildAt(i);
             final RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
             final int left = child.getRight() + params.rightMargin +
--- libraries/directoryandfilechooser/src/main/java/com/loopeer/directorychooser/DividerItemDecorationChooser.java
+++ libraries/directoryandfilechooser/src/main/java/com/loopeer/directorychooser/DividerItemDecorationChooser.java
@@ -111,7 +111,7 @@ public class DividerItemDecorationChooser extends RecyclerView.ItemDecoration {
         final int right = parent.getWidth() - parent.getPaddingRight();
 
         final int childCount = parent.getChildCount();
-        for (int i = 0; i < childCount; i++) {
+        for (int i = 0; i < childCount - 1; i++) {
             final View child = parent.getChildAt(i);
             final RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
             final int top = child.getBottom() + params.bottomMargin +
@@ -129,7 +129,7 @@ public class DividerItemDecorationChooser extends RecyclerView.ItemDecoration {
         final int bottom = parent.getHeight() - parent.getPaddingBottom();
 
         final int childCount = parent.getChildCount();
-        for (int i = 0; i < childCount; i++) {
+        for (int i = 0; i < childCount - 1; i++) {
             final View child = parent.getChildAt(i);
             final RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
             final int left = child.getRight() + params.rightMargin +
