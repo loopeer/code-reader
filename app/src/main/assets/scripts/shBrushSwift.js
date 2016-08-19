ae5ddcc78db1b407e817ffb03ddb18a2a1cf5e48
ToDou <yytodou123@gmail.com>
2016-08-19 08:21:27 +0800

    fix comments

--- app/src/main/assets/scripts/shBrushSwift.js
+++ app/src/main/assets/scripts/shBrushSwift.js
@@ -15,9 +15,11 @@
 			{ regex: SyntaxHighlighter.regexLib.singleLineCComments,	css: 'color1' },		// one line comments
 			{ regex: SyntaxHighlighter.regexLib.doubleQuotedString,		css: 'string' },		// strings
 			{ regex: SyntaxHighlighter.regexLib.singleQuotedString,		css: 'string' },		// strings
+			{ regex: /\/\*([^\*][\s\S]*)?\*\//gm,						css: 'comments' },	 	// multiline comments
+            { regex: /\/\*(?!\*\/)\*[\s\S]*?\*\//gm,					css: 'preprocessor' },	// documentation comments
 			{ regex: /\b([\d]+(\.[\d]+)?|0x[a-f0-9]+)\b/gi,				css: 'value' },			// numbers
+		    { regex: /[\+, \-, \=, \*\b, \/]/g,                           css: 'keyword' },
 			{ regex: /(?!\@interface\b)\@[\$\w]+\b/g,					css: 'keyword' },
-			{ regex: /[\+, \-, \=, \*, \/]/g,                           css: 'keyword' },
 			{ regex: new RegExp(this.getKeywords(keywords), 'gm'),		css: 'keyword' },
 			{ regex: new RegExp(this.getKeywords(values), 'gm'),		css: 'value' }	       	// java keyword
 
