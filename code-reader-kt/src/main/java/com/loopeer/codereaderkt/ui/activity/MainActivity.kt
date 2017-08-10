32be9b36f2a973c638d56c5f890c2a61e70e0bc0
owen <huangshaohuamail@gmail.com>
2017-08-10 16:13:08 +0800

    fix databinding

--- code-reader-kt/src/main/java/com/loopeer/codereaderkt/ui/activity/MainActivity.kt
+++ code-reader-kt/src/main/java/com/loopeer/codereaderkt/ui/activity/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.loopeer.codereaderkt.ui.activity
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 import com.loopeer.codereaderkt.R
+import com.loopeer.codereaderkt.databinding.ActivityMainBinding
 
 /**
  * Created by loopeer on 2017/8/10.
  */
 class MainActivity : AppCompatActivity() {
 
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
     }
 }
