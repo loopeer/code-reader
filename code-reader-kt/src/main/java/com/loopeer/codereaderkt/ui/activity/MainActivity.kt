e1e39977c0f843e9979a14bd174d7fc97c1d55e5
owen <huangshaohuamail@gmail.com>
2017-08-10 17:34:04 +0800

    add some onclick action

--- code-reader-kt/src/main/java/com/loopeer/codereaderkt/ui/activity/MainActivity.kt
+++ code-reader-kt/src/main/java/com/loopeer/codereaderkt/ui/activity/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.loopeer.codereaderkt.ui.activity
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.databinding.DataBindingUtil
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import com.loopeer.codereaderkt.R
 import com.loopeer.codereaderkt.databinding.ActivityMainBinding
 
@@ -18,6 +25,65 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(false)//？？
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                        MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE)
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_repo_add, menu)
+        menuInflater.inflate(R.menu.menu_settings, menu)
+        menuInflater.inflate(R.menu.menu_github, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+//            R.id.action_settings -> Navigator.startSettingActivity(this)
+            R.id.action_settings -> Toast.makeText(this, "action_settings", Toast.LENGTH_SHORT).show()
+//            R.id.action_repo_add -> Navigator.startAddRepoActivity(this)
+            R.id.action_repo_add -> Toast.makeText(this, "action_repo_add", Toast.LENGTH_SHORT).show()
+//            R.id.action_github -> Navigator.startLoginActivity(this)
+            R.id.action_github -> Toast.makeText(this, "action_github", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    fun onFabClick(view: View) {
+        Toast.makeText(this, "Fab Clicked !", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                }
+                return
+            }
+        }
     }
 }
