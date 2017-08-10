3aa1217162bbf18d380bd27a478e10a71d7afa07
owen <huangshaohuamail@gmail.com>
2017-08-10 15:54:09 +0800

    add databinding

---
+++ code-reader-kt/src/main/java/com/loopeer/codereaderkt/ui/activity/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.loopeer.codereaderkt.ui.activity
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+import com.loopeer.codereaderkt.R
+
+/**
+ * Created by loopeer on 2017/8/10.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
