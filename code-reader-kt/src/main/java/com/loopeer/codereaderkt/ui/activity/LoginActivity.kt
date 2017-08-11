8741961bbbd289895a01b32890b217a004615bb9
owen <huangshaohuamail@gmail.com>
2017-08-11 16:14:00 +0800

    add loginactivity

--- code-reader-kt/src/main/java/com/loopeer/codereaderkt/ui/activity/LoginActivity.kt
+++ code-reader-kt/src/main/java/com/loopeer/codereaderkt/ui/activity/LoginActivity.kt
@@ -1,7 +1,18 @@
 package com.loopeer.codereaderkt.ui.activity
 
-import android.support.v7.app.AppCompatActivity
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import com.loopeer.codereaderkt.R
+import com.loopeer.codereaderkt.databinding.ActivityLoginBinding
 
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity() {
+    lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+    }
+
+
 }
